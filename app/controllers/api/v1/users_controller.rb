4e6c7a4fd26dbadeadfd48ccb52dd48769e52c76
Nguyen Quang Lam <lam.nguyen@employmenthero.com>
2023-03-05 17:54:21 +0700

    chore: generate JWT for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,19 @@
+require 'jwt'
+
 class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(create_user_params)
     @user.password = params[:password]
     @user.save!
-    render json: @user
+
+    @jwt_payload = {id: @user.id, username: @user.username, email: @user.email}
+    @token = JWT.encode @jwt_payload, ENV['JWT_SECRET'] ,'HS512'
+
+    render json: {user: @user, token: @token }
   end
 
   def authenticate
-    
+
   end
 
   private
